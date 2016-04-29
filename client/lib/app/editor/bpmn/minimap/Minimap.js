6f6a4b2f720b98825f583d601209c963c5142ead
Ricardo Matias <ricardo.matias@camunda.com>
2016-04-29 08:36:11 +0200

    blah

---
+++ client/lib/app/editor/bpmn/minimap/Minimap.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+/**
+ * Provides ability to search through BPMN elements
+ */
+function Minimap() {
+
+}
+
+module.exports = Minimap;
+
+Minimap._inject = [];
---
+++ client/lib/app/editor/bpmn/minimap/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  __init__: [
+    require('./Minimap')
+  ]
+};
